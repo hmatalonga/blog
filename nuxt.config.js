77bf3e341a92576632dea0facc8850dd1a4203ec
Hugo Matalonga <hmatalonga@gmail.com>
2018-04-25 17:49:44 +0100

    Delete unused folders

--- nuxt.config.js
+++ nuxt.config.js
@@ -11,7 +11,7 @@ module.exports = {
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Raleway:500,400' }
+      { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css?family=Oswald:500,400' }
     ]
   },
   /*
