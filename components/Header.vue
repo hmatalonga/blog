e7e7be0e4624ba54ac297d2118383b1a607628e3
Hugo Matalonga <hmatalonga@gmail.com>
2021-03-25 18:16:05 +0000

    feat: add feed icon to navigation menu

--- components/Header.vue
+++ components/Header.vue
@@ -4,11 +4,30 @@
       <nuxt-link class="logo" to="/">Hugo Matalonga</nuxt-link>
     </div>
     <div>
-      <ul class="list-none lg:text-base xl:text-lg text-gray-600">
-        <li>
+      <div
+        class="flex items-center space-x-6 lg:space-x-8 lg:text-base xl:text-lg text-gray-600"
+      >
+        <div>
           <nuxt-link class="hover:text-gray-800" to="/blog">Blog</nuxt-link>
-        </li>
-      </ul>
+        </div>
+        <div>
+          <a href="/feed.xml">
+            <svg
+              class="current-color w-5 h-5"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 20 20"
+              fill="currentColor"
+            >
+              <path
+                d="M5 3a1 1 0 000 2c5.523 0 10 4.477 10 10a1 1 0 102 0C17 8.373 11.627 3 5 3z"
+              />
+              <path
+                d="M4 9a1 1 0 011-1 7 7 0 017 7 1 1 0 11-2 0 5 5 0 00-5-5 1 1 0 01-1-1zM3 15a2 2 0 114 0 2 2 0 01-4 0z"
+              />
+            </svg>
+          </a>
+        </div>
+      </div>
     </div>
   </header>
 </template>
